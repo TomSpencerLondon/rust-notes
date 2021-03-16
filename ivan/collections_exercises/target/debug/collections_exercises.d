cd4d56bc6e23b5acba9e6e5f643e71788e4cb908
Tom <thomas.spencer@codurance.com>
2021-03-16 19:27:42 +0000

    add files

---
+++ ivan/collections_exercises/target/debug/collections_exercises.d
@@ -0,0 +1,1 @@
+/Users/tomspencer/Desktop/rust-notes/ivan/collections_exercises/target/debug/collections_exercises: /Users/tomspencer/Desktop/rust-notes/ivan/collections_exercises/src/main.rs
---
+++ ivan/collections_exercises/target/debug/deps/collections_exercises.d
@@ -0,0 +1,5 @@
+/Users/tomspencer/Desktop/rust-notes/ivan/collections_exercises/target/debug/deps/collections_exercises: src/main.rs
+
+/Users/tomspencer/Desktop/rust-notes/ivan/collections_exercises/target/debug/deps/collections_exercises.d: src/main.rs
+
+src/main.rs:
