fd3b3459de085c18c68ff7b30f44bc86fe35bf56
Tom <thomas.spencer@codurance.com>
2021-04-20 19:15:23 +0100

    Add tests for Guess

--- ivan_testing/ch11_summary/src/lib.rs
+++ ivan_testing/ch11_summary/src/lib.rs
@@ -20,4 +20,27 @@ mod add_two_tests {
     }
 }
 
+struct Guess {
+    value: i32,
+}
 
+impl Guess {
+    fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!("Value must be greater than or equal to 1, got {}", value);
+        } else if value > 100 {
+            panic!("Value must be less than or equal to 100, got {}", value);
+        }
+
+        Guess { value }
+    }
+}
+
+#[cfg(test)]
+mod guess_tests {
+    use super::Guess;
+
+    #[test]
+    #[should_panic(expected = "Value must be less than or equal to 100")]
+    fn greater_than_100() { Guess::new(200); }
+}
