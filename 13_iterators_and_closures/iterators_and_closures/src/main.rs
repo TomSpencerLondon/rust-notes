e222196a247632d318914bd235c6c53a69855399
Tom <thomas.spencer@codurance.com>
2021-05-19 07:03:18 +0100

    add notes for chapter 13

---
+++ 13_iterators_and_closures/iterators_and_closures/src/main.rs
@@ -0,0 +1,33 @@
+//
+
+use std::time::Duration;
+use std::thread;
+use std::intrinsics::likely;
+
+fn main() {
+    println!("Hello, world!");
+
+    // let example_closure = |x| x;
+    //
+    // let s = example_closure(String::from("hello"));
+    // let n = example_closure(5);
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_closure = |num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
+    if intensity < 25 {
+        println!("today, do {} pushups!", expensive_closure(intensity));
+        println!("Next, do {} situps!", expensive_closure(intensity));
+    }else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!("Today, run for {} minutes!", expensive_closure(intensity));
+        }
+    }
+}
---
+++ 13_iterators_and_closures/iterators_and_closures/target/debug/deps/iterators_and_closures.d
@@ -0,0 +1,5 @@
+/Users/tomspencer/Desktop/rust-notes/13_iterators_and_closures/iterators_and_closures/target/debug/deps/iterators_and_closures: src/main.rs
+
+/Users/tomspencer/Desktop/rust-notes/13_iterators_and_closures/iterators_and_closures/target/debug/deps/iterators_and_closures.d: src/main.rs
+
+src/main.rs:
