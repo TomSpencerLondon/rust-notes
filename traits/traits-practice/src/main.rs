4a7b7e63bc02e76d6ae834db5d4dd6f202552707
Tom <thomas.spencer@codurance.com>
2021-05-19 21:31:40 +0100

    Add bite trait for Grape impl

--- traits/traits-practice/src/main.rs
+++ traits/traits-practice/src/main.rs
@@ -8,11 +8,21 @@ struct Grapes {
     amount_left: i32,
 }
 
+impl Bite for Grapes {
+    fn bite(self: &mut Self) {
+        self.amount_left -= 1;
+    }
+}
+
 fn main() {
     let mut carrot = Carrot { percent_left: 100.0 };
     carrot.bite();
 
     println!("I take a bit: {:?}", carrot);
+
+    let mut grapes = Grapes { amount_left: 100 };
+    grapes.bite();
+    println!("Eat a grape: {:?}", grapes);
 }
 
 #[derive(Debug)]
