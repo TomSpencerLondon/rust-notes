600395ba1d389f1bdb0f63b39b3a67ad3437273d
Tom <thomas.spencer@codurance.com>
2021-06-01 20:05:36 +0100

    add code for pointers

--- mattsi-pointers/pointers/src/main.rs
+++ mattsi-pointers/pointers/src/main.rs
@@ -1,18 +1,47 @@
-fn main() {
-    let b = Box::new(String::from("from"));
+use std::ops::Deref;
 
-    let c = &*b;
+struct MyBox<T>(T);
 
-    print!("{} {}", b, c);
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
 
-    let x = 5;
-    let y = Box::new(x);
+impl<T> Deref for MyBox<T> {
+    type Target = T;
 
-    assert_eq!(5, *y);
+    fn deref(&self) -> &T {
+        &self.0
+    }
 }
 
 
 enum List {
     Cons(i32, Box<List>),
     Nil,
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
+
+
+fn main() {
+    let b = Box::new(String::from("from"));
+
+    let c = &*b;
+
+    println!("{} {}", b, c);
+
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, *(y.deref()));
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&(*m)[..]);
 }
