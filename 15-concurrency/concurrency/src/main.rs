a9a99c2e289addecc6de0067d36f1c1a028c3316
thomas.spencer@codurance.com <thomas.spencer@codurance.com>
2021-06-07 05:55:54 +0100

    add threading

--- 15-concurrency/concurrency/src/main.rs
+++ 15-concurrency/concurrency/src/main.rs
@@ -1,6 +1,6 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
-
 fn main() {
     // let handle = thread::spawn(|| {
     //     for i in 1..10 {
@@ -15,11 +15,42 @@ fn main() {
     //     thread::sleep(Duration::from_millis(1));
     // }
 
-    let v = vec![1, 2, 3];
+    // let v = vec![1, 2, 3];
+    //
+    // let handle = thread::spawn(move || {
+    //     println!("Here's a vector: {:?}", v);
+    // });
+    //
+    // handle.join().unwrap();
 
-    let handle = thread::spawn(move || {
-        println!("Here's a vector: {:?}", v);
+    // let (tx, rx) = mpsc::channel();
+    //
+    // thread::spawn(move || {
+    //     let val = String::from("hi");
+    //     tx.send(val).unwrap();
+    //     println!("val is {}", val);
+    // });
+    //
+    // let received = rx.recv().unwrap();
+    // println!("Got: {}", received);
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+        String::from("hi"),
+        String::from("from"),
+        String::from("the"),
+        String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
-    handle.join().unwrap();
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
