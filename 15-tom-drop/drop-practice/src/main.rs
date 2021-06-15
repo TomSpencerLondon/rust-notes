ca02e3046bec91ff30b8237e2e8a16c4a1eef916
Tom <thomas.spencer@codurance.com>
2021-06-15 06:09:44 +0100

    Delete guessing game - added in error

--- 15-tom-drop/drop-practice/src/main.rs
+++ 15-tom-drop/drop-practice/src/main.rs
@@ -43,44 +43,4 @@ fn main() {
     // drop(c);
     // println!("CustomSmartPointer dropped before end of main");
 
-
-    //3.  println!("Guess the number!");
-    // println!("Please input your guess.");
-    //
-    // let mut guess = String::new();
-    //
-    // io::stdin()
-    //     .read_line(&mut guess)
-    //     .expect("Failed to read line");
-    //
-    // println!("You guessed: {}", guess);
-
-    // 4. Guess Game
-
-    // let mut secret_number = 15;
-    //
-    // loop {
-    //     println!("Please input your guess.");
-    //     let mut guess = String::new();
-    //
-    //     io::stdin()
-    //         .read_line(&mut guess)
-    //         .expect("Failed to read line");
-    //
-    //     let guess: u32 = match guess.trim().parse() {
-    //         Ok(num) => num,
-    //         Err(_) => continue,
-    //     };
-    //
-    //     println!("You guessed: {}", guess);
-    //     match guess.cmp(&secret_number) {
-    //         Ordering::Less => println!("Too small!"),
-    //         Ordering::Greater => println!("Too big!"),
-    //         Ordering::Equal => {
-    //             println!("You win!");
-    //             break;
-    //         }
-    //     }
-    // }
-
 }
