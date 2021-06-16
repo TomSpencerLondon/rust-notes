36e6af8279776bb7f2267c93ed1041ab13b154da
thomas.spencer@codurance.com <thomas.spencer@codurance.com>
2021-06-16 06:22:47 +0100

    add

--- 16-using-threads-to-run-code-simultaneously/threads/src/mutex_threads.rs
+++ 16-using-threads-to-run-code-simultaneously/threads/src/mutex_threads.rs
@@ -1,17 +1,17 @@
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
-fn main(){
-    let counter = Mutex::new(0);
+fn main() {
+    let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
     for _ in 0..10 {
+        let counter = Arc::clone(&counter);
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
 
             *num += 1;
         });
-
         handles.push(handle);
     }
 
--- 16-using-threads-to-run-code-simultaneously/threads/target/debug/deps/threads.d
+++ 16-using-threads-to-run-code-simultaneously/threads/target/debug/deps/threads.d
@@ -1,6 +1,8 @@
-/Users/tomspencer/Desktop/rust-notes/16-using-threads-to-run-code-simultaneously/threads/target/debug/deps/threads: src/main.rs src/move_closure.rs
+/Users/tomspencer/Desktop/rust-notes/16-using-threads-to-run-code-simultaneously/threads/target/debug/deps/threads: src/main.rs src/move_closure.rs src/mutex_practice.rs src/mutex_threads.rs
 
-/Users/tomspencer/Desktop/rust-notes/16-using-threads-to-run-code-simultaneously/threads/target/debug/deps/threads.d: src/main.rs src/move_closure.rs
+/Users/tomspencer/Desktop/rust-notes/16-using-threads-to-run-code-simultaneously/threads/target/debug/deps/threads.d: src/main.rs src/move_closure.rs src/mutex_practice.rs src/mutex_threads.rs
 
 src/main.rs:
 src/move_closure.rs:
+src/mutex_practice.rs:
+src/mutex_threads.rs:
